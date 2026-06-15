2e02029fc176301b5a2f38a4f56ae9bd99469d6d
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-06-15 11:15:34 +0200

    adding functionality to symptom list

--- pages/symptom_list_page.py
+++ pages/symptom_list_page.py
@@ -50,14 +50,23 @@ class SymptomListFrame(ctk.CTkFrame):
         self.frm = ctk.CTkFrame(
             self
         )
-        self.frm.pack(padx=5, pady=5, expand=True, fill="both")
+        self.frm.grid_columnconfigure([0, 1], weight=1)
+        self.frm.pack(padx=5, pady=5)
 
         self.symptom_options_menu = ctk.CTkOptionMenu(
             self.frm,
             values=self.options,
         )
         self.symptom_options_menu.set(self.parent.translator.dictionary['opt'])
-        self.symptom_options_menu.grid(row=0, column=0)
+        self.symptom_options_menu.grid(row=0, column=0, sticky="e", padx=5)
+
+        self.select_option_btn = ctk.CTkButton(
+            self.frm,
+            text=self.parent.translator.dictionary['confirm'],
+            font=self.parent.button_font,
+            command=self.make_choice,
+        )
+        self.select_option_btn.grid(row=0, column=1, sticky="w", padx=5)
 
         self.listbox = CTkListbox(
             self,
@@ -66,8 +75,6 @@ class SymptomListFrame(ctk.CTkFrame):
         self.listbox.pack(padx=5, pady=5, fill="both", expand=True)
 
         for k, v in self.symptoms_dict.items():
-            print("k: ", k)
-            print("v: ", v)
             self.listbox.insert("end", f"{k.title()}: {v}")
 
 
@@ -80,3 +87,19 @@ class SymptomListFrame(ctk.CTkFrame):
                 self.symptoms_dict[i[1]] = i[2]
         except Exception as e:
             print("Error: ", e)
+
+
+    def make_choice(self):
+        """Make choice button"""
+        choice = self.symptom_options_menu.get()
+        if choice != self.parent.translator.dictionary['opt']:
+            if choice == self.parent.translator.dictionary['edit_sympt']:
+                print("edit")
+            elif choice == self.parent.translator.dictionary['delete_sympt']:
+                print("delete")
+        else:
+            print("MAKE A CHOICE")
+
+
+    def delete_symptom(self):
+        pass
