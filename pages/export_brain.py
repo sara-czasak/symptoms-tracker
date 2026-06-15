22dd03957b091a7c2f084fb53041b8b65622aea4
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-06-15 14:49:37 +0200

    add automatic save feature

--- pages/export_brain.py
+++ pages/export_brain.py
@@ -6,37 +6,23 @@ import openpyxl
 
 
 class DataManager:
-    """MAnage gathering data and saving to excel and word"""
+    """Manage gathering data and saving to excel and word"""
     def __init__(self):
-        self.dir_path = None
-        self.diary_path = None
-        self.symptom_path = None
+        self.dir_path = os.path.abspath("./user_data")
+        self.diary_path = os.path.abspath(os.path.join(self.dir_path, 'diary.docx'))
+        self.symptom_path = os.path.abspath(os.path.join(self.dir_path, 'symptoms.xlsx'))
 
         self.log_data = {}
         self.log_detail_data = {}
 
 
     def create_dir_if_not_exists(self):
+        """Check if directory exists and if not, create it"""
         if os.path.isdir("./user_data"):
-            self.dir_path = os.path.abspath("./user_data")
-            self.diary_path = os.path.abspath(os.path.join(self.dir_path, 'diary.docx'))
-            self.symptom_path = os.path.abspath(os.path.join(self.dir_path, 'symptoms.xlsx'))
+            pass
         else:
-            os.mkdir("./user_data")
-            self.dir_path = os.path.abspath("./user_data")
-            self.diary_path = os.path.abspath(os.path.join(self.dir_path, 'diary.docx'))
-            self.symptom_path = os.path.abspath(os.path.join(self.dir_path, 'symptoms.xlsx'))
-            document = Document()
-            document.save(self.diary_path)
-            symptoms_file = openpyxl.Workbook()
-            sheet = symptoms_file.active
-            headers = ['Date', 'Symptom', 'Severity']
-            for col_num, header in enumerate(headers, start=1):
-                sheet.cell(row=1, column=col_num, value=header)
-            sheet.column_dimensions["A"].width = 20
-            sheet.column_dimensions["B"].width = 20
-            sheet.column_dimensions["C"].width = 20
-            symptoms_file.save(self.symptom_path)
+            os.mkdir(self.dir_path)
+
 
     def get_current_data_to_save(self):
         db = SymptomsDB()
@@ -45,11 +31,13 @@ class DataManager:
             for log in logs:
                 self.log_data[log[0]] = [log[1], log[3]]
             self.save_diary_data()
+            self.save_symptoms_data()
         except Exception as e:
             print("Error: ", e)
 
 
     def save_diary_data(self):
+        """Save diary data to word"""
         try:
             if os.path.exists(self.diary_path):
                 os.remove(self.diary_path)
@@ -67,6 +55,7 @@ class DataManager:
 
 
     def save_symptoms_data(self):
+        """Save symptoms data to excel"""
         db = SymptomsDB()
         for key in self.log_data.keys():
             try:
@@ -87,8 +76,20 @@ class DataManager:
 
             except Exception as e:
                 print("Error: ", e)
+
+        symptoms_file = openpyxl.Workbook()
+        sheet = symptoms_file.active
+        headers = ['Date', 'Symptom', 'Severity']
+        for col_num, header in enumerate(headers, start=1):
+            sheet.cell(row=1, column=col_num, value=header)
+        sheet.column_dimensions["A"].width = 20
+        sheet.column_dimensions["B"].width = 20
+        sheet.column_dimensions["C"].width = 20
+
         for i in self.log_detail_data.values():
-            print(i)
+            sheet.append([i[0]['date'], i[0]['name'], i[0]['level']])
+
+        symptoms_file.save(self.symptom_path)
 
 
 if __name__ == "__main__":
